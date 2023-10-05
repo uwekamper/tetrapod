5fb84f8c7fb3dae7bd507f20ee2f1ada43088bb9
Uwe Kamper <me@uwekamper.de>
2023-10-05 21:16:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='tetrapod',
     version='0.1',
     py_modules='tetrapod',
-    install_requires=['Click', 'requests', 'requests-oauthlib', 'python-dateutil>=2' ],
+    install_requires=['Click', 'requests', 'requests-oauthlib', 'python-dateutil>=2.8.0' ],
     entry_points='''
         [console_scripts]
         tpod=tetrapod.cli:cli
